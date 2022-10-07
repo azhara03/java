a09a3a67ecb3cc208581ce0898d6bdb26769172e
azhara03 <azhara.bekchoroeva@gmail.com>
2022-10-07 23:01:07 +0600

    fixed

--- week4/create.sql
+++ week4/create.sql
@@ -7,12 +7,12 @@ CREATE TABLE IF NOT EXISTS product (
     id_category    INT          NOT NULL,
     product_name   VARCHAR(30)  UNIQUE       NOT NULL,
     weight		   VARCHAR(20),
-    cost		   VARCHAR(20)  NOT NULL
+    cost		   VARCHAR(20)  
 );
 CREATE TABLE IF NOT EXISTS customer (
     id		       INT          PRIMARY KEY  AUTO_INCREMENT,
 	name           VARCHAR(15)  NOT NULL,
-    phoneNumber	   VARCHAR(20)  UNIQUE NOT NULL
+    phoneNumber	   VARCHAR(20)  UNIQUE 
 );
 CREATE TABLE IF NOT EXISTS employee (
     id		       INT          PRIMARY KEY  AUTO_INCREMENT,
@@ -20,9 +20,9 @@ CREATE TABLE IF NOT EXISTS employee (
 );
 CREATE TABLE IF NOT EXISTS sales (
     id		        INT          PRIMARY KEY  AUTO_INCREMENT,
-    id_customer     INT          NOT NULL,
-    id_employee     INT          NOT NULL,
-	id_product      INT          NOT NULL,
-    quantity	    INT          NOT NULL,
-    date_of_pay     TIMESTAMP    NOT NULL   
+    id_customer     INT,
+    id_employee     INT,
+	id_product      INT,
+    quantity	    INT,
+    date_of_pay     TIMESTAMP   
 );
