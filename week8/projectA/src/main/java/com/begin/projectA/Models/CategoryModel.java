a07c354b08ecfff717edb5438828d4310a53b415
azhara03 <azhara.bekchoroeva@gmail.com>
2022-11-06 23:59:07 +0600

    security week8

---
+++ week8/projectA/src/main/java/com/begin/projectA/Models/CategoryModel.java
@@ -0,0 +1,10 @@
+package com.begin.projectA.Models;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryModel {
+    private int id;
+    private String description;
+}
---
+++ week8/projectA/src/main/java/com/begin/projectA/Models/Role.java
@@ -0,0 +1,12 @@
+package com.begin.projectA.Models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
